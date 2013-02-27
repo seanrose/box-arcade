d8d62f845ea2cbe1f1dd7ab8bab198c4fbd3c3ec
Sean Rose <sean.andrew.rose@gmail.com>
2013-02-26 16:21:19 -0800

    added note about secret key

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
 BASE_URL = 'https://www.box.com/api/oauth2'
+# If you're running it locally, we just set a random secret key
 SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
 
 # Get Box client credentials here: http://box.com/developers/services/edit/
