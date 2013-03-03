ee23cd19f06c8899f03721dc1557e0ac02d3730b
Sean Rose <sean.andrew.rose@gmail.com>
2013-03-02 17:18:44 -0800

    included client id and secret for 'batteries included'

--- app/settings.py
+++ app/settings.py
@@ -5,5 +5,5 @@ BASE_URL = 'https://www.box.com/api/oauth2'
 SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
 
 # Get Box client credentials here: http://box.com/developers/services/edit/
-BOX_CLIENT_ID = 'hs8k9gu30co0rx5htt91gs0xcevntaxp'
-BOX_CLIENT_SECRET = 'QvTijDGniBGv7Jhk26teeS45D6do0nBH'
+BOX_CLIENT_ID = 'f80lxkg0k5viwfwsemjl3kbagpgzbd1q'
+BOX_CLIENT_SECRET = 'qzd1j4d7Fa6ybsumrZbCQ1v5IDXtn46K'
